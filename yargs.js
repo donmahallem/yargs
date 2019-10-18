55ac970791f80845bc74446a95e92471674fbe66
Mael LE GUEN <mael.leguen@laposte.fr>
2019-10-18 12:18:56 +0200

    feature(completion): remove done callback from getCompletion now it is async
    Allows 3 of the 4 broken tests to be OK back

--- yargs.js
+++ yargs.js
@@ -943,9 +943,9 @@ function Yargs (processArgs, cwd, parentRequire) {
     return self
   }
 
-  self.getCompletion = async function (args, done) {
-    argsert('<array> <function>', [args, done], arguments.length)
-    await completion.getCompletion(args, done)
+  self.getCompletion = function (args) {
+    argsert('<array>', [args], arguments.length)
+    return completion.getCompletion(args)
   }
 
   self.locale = function (locale) {
@@ -1135,13 +1135,14 @@ function Yargs (processArgs, cwd, parentRequire) {
         // we allow for asynchronous completions,
         // e.g., loading in a list of commands from an API.
         const completionArgs = args.slice(args.indexOf(`--${completion.completionKey}`) + 1)
-        await completion.getCompletion(completionArgs, (completions) => {
-          ;(completions || []).forEach((completion) => {
-            _logger.log(completion)
-          })
+        const completions = await completion.getCompletion(completionArgs)
 
-          self.exit(0)
+        ;(completions || []).forEach((completion) => {
+          _logger.log(completion)
         })
+
+        self.exit(0)
+
         return argv
       }
 
